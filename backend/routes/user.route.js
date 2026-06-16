4ce314f686802b49048d9716d125ddc1dbc5a1ee
Bobbina <bobbinamailliw@gmail.com>
2026-06-16 16:11:46 +0800

    changed

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -18,7 +18,7 @@ const globalLimitor = rateLimit({
 
 
 
-router.route('/registerYURENDONODDOS97aff521e00cc47b17dfd6577e752c588917b6df6cf25c8914179c26244b5e13a8a71951943ab7f3ee50f63d1525c332d4d26e35b005bbf2474a3cf6303f0093').post(registerUser);
+router.route('/registerDONODDOS97aff521e00cc47b17dfd6577e752c588917b6df6cf25c8914179c26244b5e13a8a71951943ab7f3ee50f63d1525c332d4d26e35b005bbf2474a3cf6303f0093').post(registerUser);
 
 router.route('/login').post(globalLimitor,loginUser);
 router.route('/logout').post(logoutuser);
